190ced3b39393e278448e3fe28ff941c0e852965
ndavuluri <ndavuluri@Samanthadavuluri4444@gmail.com>
2016-04-17 16:04:26 -0400

    Sending Actuator Object for a given sensorId match

--- WebApp/helper/trigger.js
+++ WebApp/helper/trigger.js
@@ -1,8 +1,22 @@
-var triggerApplication = function(actuatorID, actuatorValue, callback){
-  //TO-DO find document with application.actuator.actionID = actuatorID  
-  callback("hello");
-}
+
+var mongoose = require('mongoose');
+var dbModel = require('../models/applicationDB.js');
+var Application = dbModel.application;
+
+var triggerApplication = function(paramsensorID, paramsensorValue, callback){ //In beacon case we are not using the value but once we detect the beacon , actuator value is set according to the rule
+    Application.find({"sensor.sensorID": "sensor4"},
+        function(err, app){
+            if (err) console.log(err);
+            console.log(app);
+            if(app[0])
+            callback(app[0].actuator);
+            else
+                callback("Sensor  Id not found in DB");
+        });
+
+};
 
 module.exports = {
     triggerApplication: triggerApplication
-  }
+}
+
